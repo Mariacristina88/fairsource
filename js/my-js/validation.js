b2c924e000f2f1a109b0b3e79d429cd878799e71
Maria Cristina Di Termine <mariacristinadt@gmail.com>
2015-03-02 09:27:06 +0100

    Javascript improved

--- js/my-js/validation.js
+++ js/my-js/validation.js
@@ -1,61 +1,86 @@
-(function (window, document, undefined) {
+
+var module = (function (window, document, undefined) {
 
 'use strict';
 
 var form_tag = document.getElementsByClassName('check'), i,
 	validationError = false;
 
-var showError = function(elementId, text){
-	var errorElement = document.getElementById(elementId);
+var showError = function(errorElement, text){
+	//var errorElement = document.getElementById(elementId);
+	
 	errorElement.innerHTML = text;
 	if (text && text !== '') {
-		errorElement.style.display = 'block';  // if there is the error text inside <p>, than show it.
+		errorElement.style.display = 'block';  /** if there is the error text inside <p>, than show it. **/
 	}
 	else {
 		errorElement.style.display = 'none'; // if <p> is empty, don't show <p>.
 	}
 };
 
+var stringNotEmpty = function(string) {
+	if (string == '') {
+		return false;
+	} 
+	return true;
+};
+
+var validateEmail = function(email) {
+	var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (emailRegEx.test(email)) {
+		return true;
+	} return false;
+};
+
 
 var validate = function(){
-	var name = document.contactForm.fname.value;
-	var email = document.contactForm.femail.value;
-	var subject = document.contactForm.fsub.value;
-	var textarea = document.contactForm.fmessage.value;
-	var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var name = document.contactForm.fname;
+	var email = document.contactForm.femail;
+	var subject = document.contactForm.fsub;
+	var message = document.contactForm.fmessage;
+	var fields = ['fname', 'femail', 'fsub', 'fmessage'];
 
-	var errorCount = 0;
-
-	if(name === '') {
+	
+	for (var i=0; i<fields.length; i++) {
+		
+		var field = document.contactForm[fields[i]];
+		console.log(field.value);
+		if(!stringNotEmpty(field.value)) {
 		errorCount++;
-		showError('error-name', 'Please enter your name.');
-
-	} else {
-		showError('error-name');		
+		showError(document.contactForm[fields[i]], 'Please enter your' + fields[i]);
+		}
 	}
 
-	if (email === '' || !emailRegEx.test(email)) {
+	var errorCount = 0;
+/*
+	if(!stringNotEmpty(name)) {
+		errorCount++;
+		showError('error-name');
+
+	} 
+			
+	
+
+	if (!stringNotEmpty(email) || !validateEmail(email)) {
 		errorCount++;
 		showError('error-email', 'Please enter your email.');
 		
-	} else {
-		showError('error-email');
-	}
+	} 
+		
+	
 
-	if (subject === '') {
+	if (!stringNotEmpty(subject)) {
 		errorCount++;
 		showError('error-subject', 'Please enter a subject.');
-	} else {
-		showError('error-subject');
-	}
+	} 
 
-	if (textarea === '') {
+	if (!stringNotEmpty(message)) {
 		errorCount++;
 		showError('error-message', 'Please enter a message.');
-	} else {
-		showError('error-message');
-	}
+	} 
 	
+
+*/	
 	console.log(errorCount);
 	if(errorCount > 0 && validationError === false) {
 
@@ -75,5 +100,11 @@ document.getElementById('submit').addEventListener('click', function() {
 	validate();
 });
 
+return {
+	showError : showError,
+	validate : validate,
+	stringNotEmpty : stringNotEmpty,
+	validateEmail : validateEmail
+};
 
 })(window, document);
